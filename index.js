2010892b538905dd3c20ecd250cb3961786e34f5
agusvlopez <agusvlopez@gmail.com>
2022-10-25 21:53:47 -0300

    validar pistas

--- index.js
+++ index.js
@@ -90,8 +90,6 @@ const Cargar = () => {
 
     let codigoUnico;
 
-   
-    
     //validar codigo
     const validarCodigo = () => {
         
@@ -103,11 +101,46 @@ const Cargar = () => {
         return codigoUnico; 
     }
 
+    // validar pistas
+
+    // nombre pista
+    const validarNombrePista = () => {
+
+        let nombrePista = prompt("Ingrese el nombre de la pista");
+    
+        while (nombrePista.length === 0) {
+    
+          alert("Pista inválida");
+          nombrePista = prompt("Ingrese un nombre de pista válido");
+    
+        }
+        return nombrePista;
+    
+      }
+
+     //duracion pista
+      const validarDuracion = () => {
+    
+        let duracionPista = parseInt(prompt("Ingrese la duración de la pista (entre 0 y 7200 segundos inclusive)"));
+    
+        while ( isNaN(duracionPista) || duracionPista < 0 || duracionPista > 7200) {
+    
+          alert("Duracion incorrecta");
+          duracionPista = parseInt(prompt("Ingrese una duración que no supere los 7200 segundos o sea menor a 0"));
+    
+        }
+    
+        alert("Nota agregada correctamente");
+        return duracionPista;
+    
+      }
     // crear nuevo disco
     let nuevoDisco = new Disco();
     nuevoDisco.nombre = validarDisco();
     nuevoDisco.autor = validarAutor();
     nuevoDisco.codigo = validarCodigo();    
+
+
 };
 
 // Función Mostrar:
